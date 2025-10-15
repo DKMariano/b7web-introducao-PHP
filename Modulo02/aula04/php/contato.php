2704c496ccf5092cd252a6a8661be33c0db9fad7
D.  K.  Mariano <144285758+DKMariano@users.noreply.github.com>
2025-10-15 16:47:21 -0300

    add modulo 2

---
+++ Modulo02/aula04/php/contato.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <?php include 'partes/head.php';?>
+    <title>Página Contato</title>
+</head>
+<body>
+    <?php include 'partes/header.php';?>
+
+    <main>
+        <section>
+            <h2>Página Contato</h2>
+            <p>Bem-vindo à página de Contato do nosso site.</p>
+        </section>
+    </main>
+
+    <?php include 'partes/footer.php';?>
+</body>
+</html>
